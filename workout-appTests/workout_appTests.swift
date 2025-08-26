704f7696e6f0eb8187fba5444b19b6996a4ef08a
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-08-26 20:09:09 +0200

    Initial Commit

---
+++ workout-appTests/workout_appTests.swift
@@ -0,0 +1,16 @@
+//
+//  workout_appTests.swift
+//  workout-appTests
+//
+//  Created by Mathurin Sekine on 26/08/2025.
+//
+
+import Testing
+
+struct workout_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ workout-appUITests/workout_appUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  workout_appUITestsLaunchTests.swift
+//  workout-appUITests
+//
+//  Created by Mathurin Sekine on 26/08/2025.
+//
+
+import XCTest
+
+final class workout_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
